5395a4e212f3eb723a1f9188834547b0728786c6
Thomas Gleixner <tglx@linutronix.de>
2011-04-03 11:57:29 +0200

    [rt] genirq: Force interrupt thread on RT
    Force threaded_irqs and optimize the code (force_irqthreads) in regard
    to this.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    (cherry picked from commit 482434502e9ea43202ff6f8a3e6391cdc0b5f4cb)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -31,6 +31,7 @@ struct irq_desc_list {
 static DEFINE_RAW_SPINLOCK(perf_irqs_lock);
 
 #ifdef CONFIG_IRQ_FORCED_THREADING
+# ifndef CONFIG_PREEMPT_RT_BASE
 __read_mostly bool force_irqthreads;
 
 static int __init setup_forced_irqthreads(char *arg)
@@ -39,6 +40,7 @@ static int __init setup_forced_irqthreads(char *arg)
 	return 0;
 }
 early_param("threadirqs", setup_forced_irqthreads);
+# endif
 #endif
 
 static void __synchronize_hardirq(struct irq_desc *desc)
