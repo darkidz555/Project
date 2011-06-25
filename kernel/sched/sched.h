37857fbc06cc3d9ec49c0876dceec06e1fb9dc79
Thomas Gleixner <tglx@linutronix.de>
2011-06-25 09:21:04 +0200

    [rt|eval] sched: Add saved_state for tasks blocked on sleeping locks
    Spinlocks are state preserving in !RT. RT changes the state when a
    task gets blocked on a lock. So we need to remember the state before
    the lock contention. If a regular wakeup (not a RTmutex related
    wakeup) happens, the saved_state is updated to running. When the lock
    sleep is done, the saved state is restored.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    (cherry picked from commit 3ba25bcf05dae98810bbb568ce9a1427a72c24df)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>
    
    Conflicts:
    	include/linux/sched.h
    due to wake_up_process_no_notif
    	kernel/sched/core.c
    due to wake_up_process_no_notif
    	kernel/sched/sched.h
    due to #define WF_NO_NOTIFIER	0x08		/* do not notify
    governor */

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1964,6 +1964,7 @@ static inline void finish_lock_switch(struct rq *rq, struct task_struct *prev)
 #define WF_FORK		0x02		/* child wakeup after fork */
 #define WF_MIGRATED	0x4		/* internal use, task got migrated */
 #define WF_NO_NOTIFIER	0x08		/* do not notify governor */
+#define WF_LOCK_SLEEPER	0x08		/* wakeup spinlock "sleeper" */
 
 /*
  * To aid in avoiding the subversion of "niceness" due to uneven distribution
