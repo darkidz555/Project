a947c1fc0db37631abfc5ce8a78fdc0dd39a8b83
Roger Lu <roger.lu@mediatek.com>
2016-07-01 11:05:02 +0800

    [eval] PM / suspend: show workqueue state in suspend flow
    If freezable workqueue aborts suspend flow, show
    workqueue state for debug purpose.
    
    Signed-off-by: Roger Lu <roger.lu@mediatek.com>
    Acked-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit 7b776af66dc462caa7e839cc5c950a61db1f8551)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>
    
    Conflicts:
    	kernel/power/process.c
    
    add
    if (!wakeup) {
    
    to section

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -96,6 +96,10 @@ static int try_to_freeze_tasks(bool user_only)
 		       elapsed_msecs / 1000, elapsed_msecs % 1000,
 		       todo - wq_busy, wq_busy);
 
+		if (wq_busy)
+			show_workqueue_state();
+
+		if (!wakeup) {
 			read_lock(&tasklist_lock);
 			for_each_process_thread(g, p) {
 				if (p != current && !freezer_should_skip(p)
@@ -103,6 +107,7 @@ static int try_to_freeze_tasks(bool user_only)
 					sched_show_task(p);
 			}
 			read_unlock(&tasklist_lock);
+		}
 	} else {
 		pr_cont("(elapsed %d.%03d seconds) ", elapsed_msecs / 1000,
 			elapsed_msecs % 1000);
