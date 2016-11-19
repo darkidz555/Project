8348c991a7c8265ed1e5685c8c4292f83280e4ff
FrancescoCG <frank-97@hotmail.gr>
2016-11-19 07:22:39 +0200

    Add crazyactive governor (all credits to CrazyGamerGR)

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -537,6 +537,9 @@ extern struct cpufreq_governor cpufreq_gov_sched;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDUTIL)
 extern struct cpufreq_governor cpufreq_gov_schedutil;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_schedutil)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CRAZYACTIVE)
+extern struct cpufreq_governor cpufreq_gov_crazyactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_crazyactive)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
