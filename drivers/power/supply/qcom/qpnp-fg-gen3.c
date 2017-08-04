17e370b6bece154009c73a4a38c1545c5d16ebff
hkyu.park <hkyu.park@lge.com>
2017-08-04 17:14:34 +0900

    power: qpnp-fg-gen3: Fix the issue that device does not hold SoC 100%
    QCT Case: 03059280
    Bug: 63970267
    CR-fixed: 2080211
    
    Change-Id: Ic623b65e7870b3f95b83607c5d716aa42cc3c030
    Signed-off-by: hkyu.park <hkyu.park@lge.com>

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -1968,7 +1968,12 @@ static int fg_charge_full_update(struct fg_chip *chip)
 		pr_err("Error in getting msoc_raw, rc=%d\n", rc);
 		goto out;
 	}
-	msoc = DIV_ROUND_CLOSEST(msoc_raw * FULL_CAPACITY, FULL_SOC_RAW);
+
+	rc = fg_get_msoc_raw(chip, &msoc_raw);
+	if (rc < 0) {
+		pr_err("Error in getting msoc_raw, rc=%d\n", rc);
+		goto out;
+	}
 
 	fg_dbg(chip, FG_STATUS, "msoc: %d bsoc: %x health: %d status: %d full: %d\n",
 		msoc, bsoc, chip->health, chip->charge_status,
