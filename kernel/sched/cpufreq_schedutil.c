c0bd610ba285de85103fdd1f1d1bec4b9866e7a0
Joel Fernandes <joelaf@google.com>
2017-08-28 09:56:27 -0700

    UPSTREAM: cpufreq: schedutil: Make iowait boost more energy efficient
    Currently the iowait_boost feature in schedutil makes the frequency go
    to max on iowait wakeups.  This feature was added to handle a case that
    Peter described where the throughput of operations involving continuous
    I/O requests [1] is reduced due to running at a lower frequency, however
    the lower throughput itself causes utilization to be low and hence
    causing frequency to be low hence its "stuck".
    
    Instead of going to max, its also possible to achieve the same effect by
    ramping up to max if there are repeated in_iowait wakeups happening.
    This patch is an attempt to do that. We start from a lower frequency
    (policy->min) and double the boost for every consecutive iowait update
    until we reach the maximum iowait boost frequency (iowait_boost_max).
    
    I ran a synthetic test (continuous O_DIRECT writes in a loop) on an x86
    machine with intel_pstate in passive mode using schedutil. In this test
    the iowait_boost value ramped from 800MHz to 4GHz in 60ms. The patch
    achieves the desired improved throughput as the existing behavior.
    
    [1] https://patchwork.kernel.org/patch/9735885/
    
    Change-Id: I4a018434a50f4ca29ec15b03465f6dc212e54423
    Cc: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Cc: Len Brown <lenb@kernel.org>
    Cc: Rafael J. Wysocki <rjw@rjwysocki.net>
    Cc: Viresh Kumar <viresh.kumar@linaro.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Suggested-by: Peter Zijlstra <peterz@infradead.org>
    Suggested-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Joel Fernandes <joelaf@google.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -66,8 +66,8 @@ struct sugov_cpu {
 	struct sugov_policy *sg_policy;
 
 	bool iowait_boost_pending;
-	unsigned int iowait_boost;
-	unsigned int iowait_boost_max;
+	unsigned long iowait_boost;
+	unsigned long iowait_boost_max;
 	u64 last_update;
 
 	/* The fields below are only needed when sharing a policy. */
@@ -224,20 +224,6 @@ static void sugov_get_util(unsigned long *util, unsigned long *max, u64 time)
 static void sugov_set_iowait_boost(struct sugov_cpu *sg_cpu, u64 time,
 				   unsigned int flags)
 {
-	struct sugov_policy *sg_policy = sg_cpu->sg_policy;
-
-	if (!sg_policy->tunables->iowait_boost_enable)
-		return;
-
-	if (sg_cpu->iowait_boost) {
-		s64 delta_ns = time - sg_cpu->last_update;
-
-		/* Clear iowait_boost if the CPU apprears to have been idle. */
-		if (delta_ns > TICK_NSEC) {
-			sg_cpu->iowait_boost = 0;
-			sg_cpu->iowait_boost_pending = false;
-		}
-	}
 	if (flags & SCHED_CPUFREQ_IOWAIT) {
 		if (sg_cpu->iowait_boost_pending)
 			return;
@@ -251,13 +237,21 @@ static void sugov_set_iowait_boost(struct sugov_cpu *sg_cpu, u64 time,
 		} else {
 			sg_cpu->iowait_boost = sg_cpu->sg_policy->policy->min;
 		}
+	} else if (sg_cpu->iowait_boost) {
+		s64 delta_ns = time - sg_cpu->last_update;
+
+		/* Clear iowait_boost if the CPU apprears to have been idle. */
+		if (delta_ns > TICK_NSEC) {
+			sg_cpu->iowait_boost = 0;
+			sg_cpu->iowait_boost_pending = false;
+		}
 	}
 }
 
 static void sugov_iowait_boost(struct sugov_cpu *sg_cpu, unsigned long *util,
 			       unsigned long *max)
 {
-	unsigned int boost_util, boost_max;
+	unsigned long boost_util, boost_max;
 
 	if (!sg_cpu->iowait_boost)
 		return;
