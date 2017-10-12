8b49f6db320b4f9503fc2851a77bae6e78a49f05
Zhiqiang Tu <ztu@codeaurora.org>
2017-10-12 17:27:49 +0800

    clk: msm: Fix dummy clock fixed rate setting issue
    Set fixed rate in of_dummy_get since dummy_clk_dt_parser
    is not called.
    
    Change-Id: Id33be0a00a0a29100618f5fd25a917983f654025
    Signed-off-by: Zhiqiang Tu <ztu@codeaurora.org>
    (cherry picked from commit 474100bc0758191210cb2a1f5bf2989946837de0)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- drivers/clk/msm/clock-dummy.c
+++ drivers/clk/msm/clock-dummy.c
@@ -111,7 +111,6 @@ struct clk dummy_clk = {
 static void *dummy_clk_dt_parser(struct device *dev, struct device_node *np)
 {
 	struct clk *c;
-	u32 rate;
 
 	c = devm_kzalloc(dev, sizeof(*c), GFP_KERNEL);
 	if (!c) {
@@ -119,9 +118,6 @@ static void *dummy_clk_dt_parser(struct device *dev, struct device_node *np)
 		return ERR_PTR(-ENOMEM);
 	}
 	c->ops = &clk_ops_dummy;
-
-	if (!of_property_read_u32(np, "clock-frequency", &rate))
-		c->rate = rate;
 
 	return msmclk_generic_clk_init(dev, np, c);
 }
