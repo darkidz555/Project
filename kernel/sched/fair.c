04a7fcd9d6d7fbb413b54a5aa57a73114c56cbd5
Chris Redpath <chris.redpath@arm.com>
2017-10-25 17:25:20 +0100

    ANDROID: sched/fair: Select correct capacity state for energy_diff
    The util returned from group_max_util is not capped at the max util
    present in the group, so it can be larger than the capacity stored in
    the array. Ensure that when this happens, we always use the last entry
    in the array to fetch energy from.
    
    Tested with synthetics on Juno board.
    
    Bug: 38159576
    Change-Id: I89fb52fb7e68fa3e682e308acc232596672d03f7
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    Signed-off-by: joshuous <joshuous@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5608,7 +5608,6 @@ static int find_new_capacity(struct energy_env *eenv,
 
 	for (idx = 0; idx < sge->nr_cap_states; idx++) {
 		if (sge->cap_states[idx].cap >= util) {
-			/* Keep track of SG's capacity index */
 			eenv->cap_idx = idx;
 			break;
 		}
