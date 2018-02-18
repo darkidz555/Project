e7054b6b8672d33769d164225b220ae48d638383
Arjan van de Ven <arjan@linux.intel.com>
2018-02-18 23:35:41 +0000

    locking: rwsem: spin faster
    tweak rwsem owner spinning a bit
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    (cherry picked from commit d00c0e1bc1516a109c4993f078b4b2b5a0d55d29)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- kernel/locking/rwsem-xadd.c
+++ kernel/locking/rwsem-xadd.c
@@ -328,7 +328,11 @@ done:
 static noinline
 bool rwsem_spin_on_owner(struct rw_semaphore *sem, struct task_struct *owner)
 {
-	long count;
+	struct task_struct *owner = READ_ONCE(sem->owner);
+	int i = 0;
+
+	if (!rwsem_owner_is_writer(owner))
+		goto out;
 
 	rcu_read_lock();
 	while (sem->owner == owner) {
@@ -346,7 +350,8 @@ bool rwsem_spin_on_owner(struct rw_semaphore *sem, struct task_struct *owner)
 			return false;
 		}
 
-		cpu_relax_lowlatency();
+		if (i++ > 1000)
+			cpu_relax_lowlatency();
 	}
 	rcu_read_unlock();
 
