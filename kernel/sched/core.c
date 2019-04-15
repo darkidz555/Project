5da99967452d900bd760da63e732c7e152f3960a
Sultan Alsawaf <sultan@kerneltoast.com>
2019-04-14 20:24:49 -0700

    kernel: Add API to mark IRQs and kthreads as performance critical
    On devices with a CPU that contains heterogeneous cores (e.g.,
    big.LITTLE), it can be beneficial to place some performance-critical
    IRQs and kthreads onto the performance CPU cluster in order to improve
    performance.
    
    This commit adds the following APIs:
    -kthread_run_perf_critical() to create and start a perf-critical kthread
    -IRQF_PERF_CRITICAL to schedule an IRQ and any threads it may have onto
     performance CPUs
    -PF_PERF_CRITICAL to mark a process (mainly a kthread) as performance
     critical (this is used by kthread_run_perf_critical())
    
    In order to accommodate this new API, the following changes are made:
    -Performance-critical IRQs are distributed evenly among online CPUs
     available in cpu_perf_mask
    -Performance-critical IRQs have their affinities reaffined upon exit
     from suspend (since the affinities are broken when non-boot CPUs are
     disabled)
    -Performance-critical IRQs and their threads have their affinities reset
     upon entering suspend, so that upon immediate suspend exit (when only
     the boot CPU is online), interrupts can be processed and interrupt
     threads can be scheduled onto an online CPU (otherwise we'd hit a
     kernel BUG)
    -do_set_cpus_allowed() and __set_cpus_allowed_ptr() are modified to
     enforce a performance-critical kthread's affinity
    -Perf-critical IRQs are marked with IRQD_AFFINITY_MANAGED so userspace
     can't mess with their affinity
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1266,6 +1266,8 @@ static int __set_cpus_allowed_ptr(struct task_struct *p,
 
 	new_mask = adjust_cpumask(p, new_mask);
 
+	new_mask = adjust_cpumask(p, new_mask);
+
 	rq = task_rq_lock(p, &flags);
 
 	/*
