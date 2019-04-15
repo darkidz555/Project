0ab06bb3111403c62f9180b64668a6959f5c1cb5
Sultan Alsawaf <sultan@kerneltoast.com>
2019-04-14 18:17:42 -0700

    input: touchscreen: Offload fb notifier callbacks onto workers
    Framebuffer notifier callbacks block framebuffer state changes until
    the callbacks are finished executing. This means that any significant
    delays in a framebuffer notifier callback will stall the screen when
    powering it on.
    
    In order to eliminate screen-on delays from these framebuffer notifiers,
    offload their callbacks onto workers.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/input/touchscreen/stm/ftm4_ts.c
+++ drivers/input/touchscreen/stm/ftm4_ts.c
@@ -89,6 +89,8 @@ static int fts_suspend(struct i2c_client *client, pm_message_t mesg);
 static int fts_resume(struct i2c_client *client);
 
 #if defined(CONFIG_FB)
+static void touch_resume_worker(struct work_struct *work);
+static void touch_suspend_worker(struct work_struct *work);
 static int touch_fb_notifier_callback(struct notifier_block *self,
 		unsigned long event, void *data);
 #endif
@@ -1643,7 +1645,9 @@ static int fts_probe(struct i2c_client *client, const struct i2c_device_id *idp)
 		__func__, info->irq);
 #endif
 
-#if defined(CONFIG_FB)
+#ifdef CONFIG_FB
+	INIT_WORK(&info->resume_work, touch_resume_worker);
+	INIT_WORK(&info->suspend_work, touch_suspend_worker);
 	info->fb_notif.notifier_call = touch_fb_notifier_callback;
 	retval = fb_register_client(&info->fb_notif);
 #endif
@@ -2127,6 +2131,22 @@ static int fts_resume(struct i2c_client *client)
 }
 
 #if defined(CONFIG_FB)
+static void touch_resume_worker(struct work_struct *work)
+{
+	struct fts_ts_info *info = container_of(work, typeof(*info),
+						resume_work);
+
+	fts_resume(info->client);
+}
+
+static void touch_suspend_worker(struct work_struct *work)
+{
+	struct fts_ts_info *info = container_of(work, typeof(*info),
+						suspend_work);
+
+	fts_suspend(info->client, PMSG_SUSPEND);
+}
+
 static int touch_fb_notifier_callback(struct notifier_block *self,
 		unsigned long event, void *data)
 {
@@ -2136,11 +2156,13 @@ static int touch_fb_notifier_callback(struct notifier_block *self,
 
 	if (ev && ev->data && event == FB_EVENT_BLANK) {
 		int *blank = (int *)ev->data;
-
-		if (*blank == FB_BLANK_UNBLANK)
-			fts_resume(info->client);
-		else if (*blank == FB_BLANK_POWERDOWN)
-			fts_suspend(info->client, PMSG_SUSPEND);
+		if (event == FB_EARLY_EVENT_BLANK && *blank != FB_BLANK_UNBLANK) {
+			flush_work(&info->resume_work);
+			schedule_work(&info->suspend_work);
+		} else if (event == FB_EVENT_BLANK && *blank == FB_BLANK_UNBLANK) {
+			flush_work(&info->suspend_work);
+			schedule_work(&info->resume_work);
+		}
 	}
 
 	return 0;
