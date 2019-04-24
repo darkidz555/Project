ede2091986a6926d7e6e4d5630e176af611e192b
zachariasmaladroit <zacharias.maladroit@gmail.com>
2019-04-24 17:55:08 +0200

    drivers/staging/qcacld-3.0/core/sme/src/csr/csr_neighbor_roam.c: fix Werror=misleading-indentation in csr_neighbor_roam_info_ctx_init
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- drivers/staging/qcacld-3.0/core/sme/src/csr/csr_neighbor_roam.c
+++ drivers/staging/qcacld-3.0/core/sme/src/csr/csr_neighbor_roam.c
@@ -1038,7 +1038,7 @@ static void csr_neighbor_roam_info_ctx_init(
 				qdf_mem_free(pMac->roam.pReassocResp);
 				pMac->roam.pReassocResp = NULL;
 			}
-		} else
+		} else {
 #endif
 		{
 			csr_roam_offload_scan(pMac, session_id,
