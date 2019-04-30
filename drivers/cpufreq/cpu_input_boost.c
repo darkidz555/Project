563167e2012a2f5d78f270787a94a0e796cb1246
LibXZR <xzr467706992@163.com>
2019-04-30 19:40:11 +0800

    cpu_input_boost: Implement boost_control

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -10,42 +10,7 @@
 #include <linux/fb.h>
 #include <linux/input.h>
 #include <linux/kthread.h>
-#include <linux/moduleparam.h>
-#include <linux/sched.h>
-#include <linux/power_hal.h>
-
-static unsigned int input_boost_freq_lp __read_mostly =
-	CONFIG_INPUT_BOOST_FREQ_LP;
-static unsigned int input_boost_freq_hp __read_mostly =
-	CONFIG_INPUT_BOOST_FREQ_PERF;
-static unsigned int boost_min_freq_lp __read_mostly =
-	CONFIG_BASE_BOOST_FREQ_LP;
-static unsigned int boost_min_freq_hp __read_mostly =
-	CONFIG_BASE_BOOST_FREQ_PERF;
-static unsigned int max_boost_freq_lp __read_mostly =
-	CONFIG_MAX_BOOST_FREQ_LP;
-static unsigned int max_boost_freq_hp __read_mostly =
-	CONFIG_MAX_BOOST_FREQ_PERF;
-
-static unsigned short powerhal_boost_duration __read_mostly =
-	CONFIG_POWERHAL_BOOST_DURATION_MS;
-static unsigned short input_boost_duration __read_mostly =
-	CONFIG_INPUT_BOOST_DURATION_MS;
-static unsigned short wake_boost_duration __read_mostly =
-	CONFIG_WAKE_BOOST_DURATION_MS;
-
-module_param(input_boost_freq_lp, uint, 0644);
-module_param(input_boost_freq_hp, uint, 0644);
-module_param_named(remove_input_boost_freq_lp,
-	boost_min_freq_lp, uint, 0644);
-module_param_named(remove_input_boost_freq_perf,
-	boost_min_freq_hp, uint, 0644);
-module_param(max_boost_freq_lp, uint, 0644);
-module_param(max_boost_freq_hp, uint, 0644);
-
-module_param(powerhal_boost_duration, short, 0644);
-module_param(input_boost_duration, short, 0644);
-module_param(wake_boost_duration, short, 0644);
+#include <linux/boost_control.h>
 
 enum {
 	SCREEN_OFF,
@@ -88,11 +53,17 @@ static struct boost_drv boost_drv_g __read_mostly = {
 static unsigned int get_input_boost_freq(struct cpufreq_policy *policy)
 {
 	unsigned int freq;
-
+	#if CONFIG_BOOST_CONTROL
 	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
 		freq = input_boost_freq_lp;
 	else
-		freq = input_boost_freq_hp;
+		freq = input_boost_freq_perf;
+	#else
+	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
+		freq = input_boost_freq_lp;
+	else
+		freq = CONFIG_INPUT_BOOST_FREQ_PERF;
+	#endif
 
 	return min(freq, policy->max);
 }
@@ -402,7 +373,11 @@ static int fb_notifier_cb(struct notifier_block *nb, unsigned long action,
 	/* Boost when the screen turns on and unboost when it turns off */
 	if (*blank == FB_BLANK_UNBLANK) {
 		clear_bit(SCREEN_OFF, &b->state);
+		#if CONFIG_BOOST_CONTROL
 		__cpu_input_boost_kick_max(b, wake_boost_duration);
+		#else
+		__cpu_input_boost_kick_max(b, CONFIG_WAKE_BOOST_DURATION_MS);
+		#endif
 	} else {
 		set_bit(SCREEN_OFF, &b->state);
 		wake_up(&b->boost_waitq);
