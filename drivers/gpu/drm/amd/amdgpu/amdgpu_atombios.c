34945f08208fa0610f4c173e785ae3725d2beeab
vz <vz@gmail.com>
2019-05-04 04:14:46 +0000

    tweak touch and input more responsive and sensetive

--- drivers/gpu/drm/amd/amdgpu/amdgpu_atombios.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_atombios.c
@@ -678,13 +678,13 @@ int amdgpu_atombios_get_clock_info(struct amdgpu_device *adev)
 			le32_to_cpu(firmware_info->info_21.ulDefaultDispEngineClkFreq);
 		/* set a reasonable default for DP */
 		if (adev->clock.default_dispclk < 53900) {
-			DRM_INFO("Changing default dispclk from %dMhz to 600Mhz\n",
+			DRM_INFO("Changing default dispclk from %dMhz to 1000Mhz\n",
+				 adev->clock.default_dispclk / 600);
+			adev->clock.default_dispclk = 100000;
+		} else if (adev->clock.default_dispclk <= 100000) {
+			DRM_INFO("Changing default dispclk from %dMhz to 1025Mhz\n",
 				 adev->clock.default_dispclk / 100);
-			adev->clock.default_dispclk = 60000;
-		} else if (adev->clock.default_dispclk <= 60000) {
-			DRM_INFO("Changing default dispclk from %dMhz to 625Mhz\n",
-				 adev->clock.default_dispclk / 100);
-			adev->clock.default_dispclk = 62500;
+			adev->clock.default_dispclk = 102500;
 		}
 		adev->clock.dp_extclk =
 			le16_to_cpu(firmware_info->info_21.usUniphyDPModeExtClkFreq);
