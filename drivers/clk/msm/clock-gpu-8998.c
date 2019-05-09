ab2ac919eef3af97c1d8ab9de7166ea63f3a07bc
dk <dk@gmail.com>
2019-05-09 08:10:54 +0000

    Revert "tweak gpu clk rate in gpucc-8998.c"
    This reverts commit 600ace3730f6703e521b1f8107ad815fbf9f1000.

--- drivers/clk/msm/clock-gpu-8998.c
+++ drivers/clk/msm/clock-gpu-8998.c
@@ -171,9 +171,9 @@ static struct clk_freq_tbl ftbl_gfx3d_clk_src_v2[] = {
 	F_SLEW( 342000000,  684000000, gpu_pll0_pll_out_even,    1, 0, 0),
 	F_SLEW( 414000000,  828000000, gpu_pll0_pll_out_even,    1, 0, 0),
 	F_SLEW( 515000000, 1030000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 696000000, 1192000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 770000000, 1340000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 850000000, 1420000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 596000000, 1192000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 670000000, 1340000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 750000000, 1420000000, gpu_pll0_pll_out_even,    1, 0, 0),
 	F_END
 };
 
