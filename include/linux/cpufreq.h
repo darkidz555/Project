68170fe6c335c291e0483782129e59d7a60061dd
pejuangmakanan <pejuangmakanan@gmail.com>
2019-05-18 21:52:25 +0000

    Add Governor Intelliactive

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -560,6 +560,9 @@ extern struct cpufreq_governor cpufreq_gov_impulse;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
 extern struct cpufreq_governor cpufreq_gov_darkness;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
+extern struct cpufreq_governor cpufreq_gov_intelliactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intelliactive)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
