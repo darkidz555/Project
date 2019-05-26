5b657215a4d1d0a496dcad52c9b78e1b458d525a
vz <vz@gmail.com>
2019-05-26 04:08:27 +0000

    OC gpu max 827

--- drivers/clk/msm/clock-gpu-8998.c
+++ drivers/clk/msm/clock-gpu-8998.c
@@ -167,13 +167,13 @@ static struct clk_freq_tbl ftbl_gfx3d_clk_src[] = {
 
 static struct clk_freq_tbl ftbl_gfx3d_clk_src_v2[] = {
 	F_SLEW( 180000000,  360000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 257000000,  514000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 342000000,  684000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 414000000,  828000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 515000000, 1030000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 596000000, 1192000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 670000000, 1340000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 850000000, 1420000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 274000000,  514000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 362000000,  684000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 418000000,  828000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 527000000, 1030000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 616000000, 1192000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 720000000, 1340000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 827000000, 1420000000, gpu_pll0_pll_out_even,    1, 0, 0),
 	F_END
 };
 
