13173af4b1e0fb18ea19723f01ea14239a82c20e
vz <vz@gmail.com>
2019-06-16 17:26:39 +0700

    UC UV :
    due to high clock for gpu it always throttle and make game unplayable
    so underclock plus undervolt -350mv no more.heat and throttle plus it stable the game.

--- drivers/clk/msm/clock-gpu-8998.c
+++ drivers/clk/msm/clock-gpu-8998.c
@@ -166,14 +166,13 @@ static struct clk_freq_tbl ftbl_gfx3d_clk_src[] = {
 };
 
 static struct clk_freq_tbl ftbl_gfx3d_clk_src_v2[] = {
-	F_SLEW( 180000000,  360000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 257000000,  514000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 342000000,  684000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 414000000,  828000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 515000000, 1030000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 596000000, 1192000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 670000000, 1340000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 750000000, 1420000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 200000000,  400000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 250000000,  500000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 320000000,  640000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 420000000,  840000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 500000000, 1000000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 580000000, 1160000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 650000000, 1300000000, gpu_pll0_pll_out_even,    1, 0, 0),
 	F_END
 };
 
