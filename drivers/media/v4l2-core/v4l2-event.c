1c97b6ad43cafba649175d3b7eb95c2e104bd6c8
vz <vz@gmail.com>
2019-07-10 15:46:27 +0000

    Revert "media: v4l: event: Add subscription to list before calling "add" operation"
    This reverts commit 251d1206ecd49e2b4afd85e5776c9de26f3ee9b2.

--- drivers/media/v4l2-core/v4l2-event.c
+++ drivers/media/v4l2-core/v4l2-event.c
@@ -205,22 +205,6 @@ int v4l2_event_pending(struct v4l2_fh *fh)
 }
 EXPORT_SYMBOL_GPL(v4l2_event_pending);
 
-static void __v4l2_event_unsubscribe(struct v4l2_subscribed_event *sev)
-{
-	struct v4l2_fh *fh = sev->fh;
-	unsigned int i;
-
-	lockdep_assert_held(&fh->subscribe_lock);
-	assert_spin_locked(&fh->vdev->fh_lock);
-
-	/* Remove any pending events for this subscription */
-	for (i = 0; i < sev->in_use; i++) {
-		list_del(&sev->events[sev_pos(sev, i)].list);
-		fh->navailable--;
-	}
-	list_del(&sev->list);
-}
-
 int v4l2_event_subscribe(struct v4l2_fh *fh,
 			 const struct v4l2_event_subscription *sub, unsigned elems,
 			 const struct v4l2_subscribed_event_ops *ops)
@@ -254,17 +238,20 @@ int v4l2_event_subscribe(struct v4l2_fh *fh,
 
 	if (found_ev) {
 		kfree(sev);
-	} else if (sev->ops && sev->ops->add) {
-		ret = sev->ops->add(sev, elems);
+		return 0; /* Already listening */
+	}
+
+	if (sev->ops && sev->ops->add) {
+		int ret = sev->ops->add(sev, elems);
 		if (ret) {
-			spin_lock_irqsave(&fh->vdev->fh_lock, flags);
-			__v4l2_event_unsubscribe(sev);
-			spin_unlock_irqrestore(&fh->vdev->fh_lock, flags);
-			kfree(sev);
+			sev->ops = NULL;
+			v4l2_event_unsubscribe(fh, sub);
+			return ret;
 		}
 	}
 
-	mutex_unlock(&fh->subscribe_lock);
+	/* Mark as ready for use */
+	sev->elems = elems;
 
 	return 0;
 }
@@ -298,6 +285,7 @@ int v4l2_event_unsubscribe(struct v4l2_fh *fh,
 {
 	struct v4l2_subscribed_event *sev;
 	unsigned long flags;
+	int i;
 
 	if (sub->type == V4L2_EVENT_ALL) {
 		v4l2_event_unsubscribe_all(fh);
@@ -307,8 +295,14 @@ int v4l2_event_unsubscribe(struct v4l2_fh *fh,
 	spin_lock_irqsave(&fh->vdev->fh_lock, flags);
 
 	sev = v4l2_event_subscribed(fh, sub->type, sub->id);
-	if (sev != NULL)
-		__v4l2_event_unsubscribe(sev);
+	if (sev != NULL) {
+		/* Remove any pending events for this subscription */
+		for (i = 0; i < sev->in_use; i++) {
+			list_del(&sev->events[sev_pos(sev, i)].list);
+			fh->navailable--;
+		}
+		list_del(&sev->list);
+	}
 
 	spin_unlock_irqrestore(&fh->vdev->fh_lock, flags);
 
