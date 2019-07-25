bc47abae19d18550aedd01ec8310b3479a33a631
vz <vz@gmail.com>
2019-07-25 13:23:44 +0000

    Revert "tcp/dccp: drop SYN packets if accept queue is full"
    This reverts commit 4a22106512d54a63f72c921e4e5a61a261f98fc2.

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -6740,7 +6740,21 @@ int tcp_conn_request(struct request_sock_ops *rsk_ops,
 	 * limitations, they conserve resources and peer is
 	 * evidently real one.
 
-	if (sk_acceptq_is_full(sk)) {
+	 */
+	if ((sysctl_tcp_syncookies == 2 ||
+	     inet_csk_reqsk_queue_is_full(sk)) && !isn) {
+		want_cookie = tcp_syn_flood_action(sk, skb, rsk_ops->slab_name);
+		if (!want_cookie)
+			goto drop;
+	}
+#endif
+
+	/* Accept backlog is full. If we have already queued enough
+	 * of warm entries in syn queue, drop request. It is better than
+	 * clogging syn queue with openreqs with exponentially increasing
+	 * timeout.
+	 */
+	if (sk_acceptq_is_full(sk) && inet_csk_reqsk_queue_young(sk) > 1) {
 		NET_INC_STATS_BH(sock_net(sk), LINUX_MIB_LISTENOVERFLOWS);
 		goto drop;
 	}
