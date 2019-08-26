ef8ccc372c9a355347eaf3583a2e230554e1ff04
vz <dk@gmail.com>
2019-08-26 10:17:29 +0000

    rm -rf out

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#1 SMP PREEMPT Sat Aug 17 05:19:32 UTC 2019"
-#define LINUX_COMPILE_BY "dk"
-#define LINUX_COMPILE_HOST "tachyon"
-#define LINUX_COMPILER "gcc version 8.3.0 (Ubuntu/Linaro 8.3.0-6ubuntu1) "
