c2035d7648995e8c5e05d417ef5ecaa354504487
vz <dk@gmail.com>
2019-09-11 11:37:21 +0000

    Revert "sched/fair: Introduce sysctl interface for SchedTune cluster bias"
    This reverts commit 92bb33002f4819e3456faaa99ab55e6fa28730ab.

--- kernel/sched/debug.c
+++ kernel/sched/debug.c
@@ -410,7 +410,6 @@ static void sched_debug_header(struct seq_file *m)
 	PN(sysctl_sched_min_granularity);
 	PN(sysctl_sched_wakeup_granularity);
 	P(sysctl_sched_child_runs_first);
-	P(sysctl_sched_cpu_schedtune_bias);
 	P(sysctl_sched_features);
 #ifdef CONFIG_SCHED_HMP
 	P(sched_upmigrate);
