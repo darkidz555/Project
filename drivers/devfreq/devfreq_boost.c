65945d82e14c917cd7963b617d0cec37bdb2281d
vz <vz@gmail.com>
2019-10-22 10:52:23 +0000

    Revert "Expose Devfreq boost tuneables"
    This reverts commit 4f4af9c857ab28c390f7e8daa4ec949627f96fa6.

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -10,15 +10,11 @@
 #include <linux/input.h>
 #include <linux/kthread.h>
 
-#include <linux/moduleparam.h>
-
-static unsigned short input_boost_duration = CONFIG_DEVFREQ_INPUT_BOOST_DURATION_MS;
-static unsigned short wake_boost_duration = CONFIG_DEVFREQ_WAKE_BOOST_DURATION_MS;
-static unsigned int msm_cpubw_boost_freq = CONFIG_DEVFREQ_MSM_CPUBW_BOOST_FREQ;
-
-module_param(input_boost_duration, short, 0644);
-module_param(wake_boost_duration, short, 0644);
-module_param(msm_cpubw_boost_freq, uint, 0644);
+enum {
+	SCREEN_OFF,
+	INPUT_BOOST,
+	MAX_BOOST
+};
 
 struct boost_dev {
 	struct devfreq *df;
@@ -129,8 +125,8 @@ static void devfreq_max_unboost(struct work_struct *work)
 	struct boost_dev *b = container_of(to_delayed_work(work),
 					   typeof(*b), max_unboost);
 
-	queue_delayed_work(b->wq, &b->input_unboost,
-		msecs_to_jiffies(input_boost_duration));
+	clear_bit(MAX_BOOST, &b->state);
+	wake_up(&b->boost_waitq);
 }
 
 static void devfreq_update_boosts(struct boost_dev *b, unsigned long state)
@@ -193,11 +189,14 @@ static int fb_notifier_cb(struct notifier_block *nb, unsigned long action,
 	for (i = 0; i < DEVFREQ_MAX; i++) {
 		struct boost_dev *b = d->devices + i;
 
-		for (i = 0; i < DEVFREQ_MAX; i++)
-			__devfreq_boost_kick_max(d->devices + i,
-				wake_boost_duration);
-	} else {
-		devfreq_unboost_all(d);
+		if (*blank == FB_BLANK_UNBLANK) {
+			clear_bit(SCREEN_OFF, &b->state);
+			__devfreq_boost_kick_max(b,
+				CONFIG_DEVFREQ_WAKE_BOOST_DURATION_MS);
+		} else {
+			set_bit(SCREEN_OFF, &b->state);
+			wake_up(&b->boost_waitq);
+		}
 	}
 
 	return NOTIFY_OK;
@@ -304,9 +303,6 @@ static int __init devfreq_boost_init(void)
 			goto stop_kthreads;
 		}
 	}
-
-	d->devices[DEVFREQ_MSM_CPUBW].boost_freq =
-		msm_cpubw_boost_freq;
 
 	devfreq_boost_input_handler.private = d;
 	ret = input_register_handler(&devfreq_boost_input_handler);
