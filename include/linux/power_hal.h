b3e939cff59bfe1523516afb1527d3502b2ce66c
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-11-25 21:57:42 +0530

    powerhal: make it possible to do Google PowerHAL GPU boosts
    Paths:
    GPUForceRailOn: /sys/class/kgsl/kgsl-3d0/force_rail_on
    GPUForceClkOn: /sys/class/kgsl/kgsl-3d0/force_clk_on
    GPUIdleTimer: /sys/class/kgsl/kgsl-3d0/idle_timer
    
    Command:
    while true; do cat <path>; sleep 0.1; done
    
    Behaviour: Toggles correctly.
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>

--- include/linux/power_hal.h
+++ include/linux/power_hal.h
@@ -27,4 +27,21 @@ static inline void set_hist_memory_val(int val) { }
 static inline void set_hyst_length_val(int val) { }
 #endif
 
+/* UFS Boosting */
+void set_ufshcd_clkgate_enable_status(u32 value);
+
+/* Video Playback detection */
+extern bool video_streaming;
+void video_streaming_disable_schedtune(void);
+
+/* GPU Boosting */
+enum kgsl_pwrctrl_timer_type {
+	KGSL_PWR_IDLE_TIMER,
+};
+
+void __force_on_store_ph(unsigned int val, int flag);
+void __timer_store_ph(unsigned int val,
+				enum kgsl_pwrctrl_timer_type timer);
+
+>>>>>>> 5e1d325a0cbb... powerhal: make it possible to do Google PowerHAL GPU boosts
 #endif /* _POWER_HAL_H */
