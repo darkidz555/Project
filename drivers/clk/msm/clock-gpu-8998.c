298dbf16d3a5cdb9161a29445a3a921c2fbda220
dk <dk@gmail.com>
2020-01-02 07:05:38 +0000

    OC gpu 820mhz

--- drivers/clk/msm/clock-gpu-8998.c
+++ drivers/clk/msm/clock-gpu-8998.c
@@ -166,14 +166,14 @@ static struct clk_freq_tbl ftbl_gfx3d_clk_src[] = {
 };
 
 static struct clk_freq_tbl ftbl_gfx3d_clk_src_v2[] = {
-	F_SLEW( 180000000,  360000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 257000000,  514000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 342000000,  684000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 414000000,  828000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 515000000, 1030000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 596000000, 1192000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 690000000, 1380000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 780000000, 1560000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 327000000,  654000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 380000000,  760000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 450000000,  900000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 550000000, 1100000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 620000000, 1240000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 680000000, 1360000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 750000000, 1500000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 820000000, 1640000000, gpu_pll0_pll_out_even,	 1, 0, 0),
 	F_END
 };
 
@@ -606,13 +606,13 @@ static struct clk_lookup msm_clocks_gfxcc_8998[] = {
 
 static void msm_gfxcc_hamster_fixup(void)
 {
-	gpu_pll0_pll.c.fmax[VDD_DIG_MIN] = 1560000500;
+	gpu_pll0_pll.c.fmax[VDD_DIG_MIN] = 1640000500;
 	gfx3d_clk_src.freq_tbl = ftbl_gfx3d_clk_src_vq;
 }
 
 static void msm_gfxcc_8998_v2_fixup(void)
 {
-	gpu_pll0_pll.c.fmax[VDD_DIG_MIN] = 1560000500;
+	gpu_pll0_pll.c.fmax[VDD_DIG_MIN] = 1640000500;
 	gfx3d_clk_src.freq_tbl = ftbl_gfx3d_clk_src_v2;
 }
 
