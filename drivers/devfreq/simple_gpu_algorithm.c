8329fc0c355ea5bf5716e85baf37dfedc394ecb7
dk <dk@gmail.com>
2020-01-03 08:09:57 +0000

    Revert "tweak sga more performance"
    This reverts commit 5b6a704f21ab4f1d8abec760a880d18c917057e3.

--- drivers/devfreq/simple_gpu_algorithm.c
+++ drivers/devfreq/simple_gpu_algorithm.c
@@ -19,13 +19,13 @@
 #include <linux/devfreq.h>
 #include <linux/msm_adreno_devfreq.h>
 
-static int default_laziness = 1;
+static int default_laziness = 4;
 module_param_named(simple_laziness, default_laziness, int, 0664);
 
-static int ramp_up_threshold = 9000;
+static int ramp_up_threshold = 5000;
 module_param_named(simple_ramp_threshold, ramp_up_threshold, int, 0664);
 
-int simple_gpu_active = 1;
+int simple_gpu_active = 0;
 module_param_named(simple_gpu_activate, simple_gpu_active, int, 0664);
 
 static int laziness;
