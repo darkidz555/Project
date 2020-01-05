7bf727419b6fb8e5f7d91c613541bc34c543dc58
dk <dk@gmail.com>
2020-01-05 17:14:46 +0000

    Revert "OC gpu 850"
    This reverts commit b817495a8b76bb96589acba47a9bd2f3409ac6eb.

--- drivers/clk/msm/clock-gpu-8998.c
+++ drivers/clk/msm/clock-gpu-8998.c
@@ -172,8 +172,8 @@ static struct clk_freq_tbl ftbl_gfx3d_clk_src_v2[] = {
 	F_SLEW( 550000000, 1100000000, gpu_pll0_pll_out_even,    1, 0, 0),
 	F_SLEW( 620000000, 1240000000, gpu_pll0_pll_out_even,    1, 0, 0),
 	F_SLEW( 680000000, 1360000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 770000000, 1540000000, gpu_pll0_pll_out_even,    1, 0, 0),
-	F_SLEW( 850000000, 1700000000, gpu_pll0_pll_out_even,	 1, 0, 0),
+	F_SLEW( 750000000, 1500000000, gpu_pll0_pll_out_even,    1, 0, 0),
+	F_SLEW( 820000000, 1640000000, gpu_pll0_pll_out_even,	 1, 0, 0),
 	F_END
 };
 
@@ -606,13 +606,13 @@ static struct clk_lookup msm_clocks_gfxcc_8998[] = {
 
 static void msm_gfxcc_hamster_fixup(void)
 {
-	gpu_pll0_pll.c.fmax[VDD_DIG_MIN] = 1700000500;
+	gpu_pll0_pll.c.fmax[VDD_DIG_MIN] = 1640000500;
 	gfx3d_clk_src.freq_tbl = ftbl_gfx3d_clk_src_vq;
 }
 
 static void msm_gfxcc_8998_v2_fixup(void)
 {
-	gpu_pll0_pll.c.fmax[VDD_DIG_MIN] = 1700000500;
+	gpu_pll0_pll.c.fmax[VDD_DIG_MIN] = 1640000500;
 	gfx3d_clk_src.freq_tbl = ftbl_gfx3d_clk_src_v2;
 }
 
